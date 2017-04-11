df86caf8034389f29a718a41689a4644dfc99e3f
Viktor Kostov <viktor.kostov@fortnox.se>
2017-04-11 20:15:09 +0200

    update grav

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit7d4f6cb9aac739251be6dcdcec6b8aeb::getLoader();
+return ComposerAutoloaderInit1ae1f24a0195b90de4d5c22a1e5455dc::getLoader();
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -4,7 +4,7 @@
 
 namespace Composer\Autoload;
 
-class ComposerStaticInit7d4f6cb9aac739251be6dcdcec6b8aeb
+class ComposerStaticInit1ae1f24a0195b90de4d5c22a1e5455dc
 {
     public static $files = array (
         '0e6d7bf4a5811bfa5cf40c5ccd6fae6a' => __DIR__ . '/..' . '/symfony/polyfill-mbstring/bootstrap.php',
@@ -1045,11 +1045,11 @@ class ComposerStaticInit7d4f6cb9aac739251be6dcdcec6b8aeb
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->prefixLengthsPsr4 = ComposerStaticInit7d4f6cb9aac739251be6dcdcec6b8aeb::$prefixLengthsPsr4;
-            $loader->prefixDirsPsr4 = ComposerStaticInit7d4f6cb9aac739251be6dcdcec6b8aeb::$prefixDirsPsr4;
-            $loader->fallbackDirsPsr4 = ComposerStaticInit7d4f6cb9aac739251be6dcdcec6b8aeb::$fallbackDirsPsr4;
-            $loader->prefixesPsr0 = ComposerStaticInit7d4f6cb9aac739251be6dcdcec6b8aeb::$prefixesPsr0;
-            $loader->classMap = ComposerStaticInit7d4f6cb9aac739251be6dcdcec6b8aeb::$classMap;
+            $loader->prefixLengthsPsr4 = ComposerStaticInit1ae1f24a0195b90de4d5c22a1e5455dc::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit1ae1f24a0195b90de4d5c22a1e5455dc::$prefixDirsPsr4;
+            $loader->fallbackDirsPsr4 = ComposerStaticInit1ae1f24a0195b90de4d5c22a1e5455dc::$fallbackDirsPsr4;
+            $loader->prefixesPsr0 = ComposerStaticInit1ae1f24a0195b90de4d5c22a1e5455dc::$prefixesPsr0;
+            $loader->classMap = ComposerStaticInit1ae1f24a0195b90de4d5c22a1e5455dc::$classMap;
 
         }, null, ClassLoader::class);
     }
